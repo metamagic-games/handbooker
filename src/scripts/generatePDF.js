ae31180ca682561948d6178efaba3d37390497e9
Maxi Clayton Clowes <maxi@codogo.io>
2018-06-19 12:01:50 +0100

    fix(structure): In progress

--- src/scripts/generatePDF.js
+++ src/scripts/generatePDF.js
@@ -3,35 +3,35 @@ import pdf from "html-pdf";
 import generateHTML from "./generateHTML.js";
 
 const markdownOptions = {
-	encoding: "utf8"
+	encoding: "utf8",
 };
 
 const pdfOptions = {
 	format: "letter",
 	orientation: "portrait",
 	border: 0,
-	base: "../"
+	base: "../",
 };
 
-export default class generatePDF {
-	createHandbook( options, ) {
-		const html = generateHTML.parse("Hello world", options.target);
+const generatePDF = (url, options) => {
+	const html = generateHTML.parse("Hello world", options.target);
 
-		if (options.debug) {
-			console.log("Saving interim HTML...");
+	if (options.debug) {
+		console.log("Saving interim HTML...");
 
-			fs.writeFile("debug.html", html, function(err) {
-				if (err) console.log(err);
-			});
-		}
-
-		console.log("Creating PDF...");
-
-		pdf.create(html, pdfOptions).toFile( 
-			options.destination,
-			function(err) {
-				if (err) return console.log(err);
-			}
-		);
+		fs.writeFile("debug.html", html, function(err) {
+			if (err) console.log(err);
+		});
 	}
+
+	console.log("Creating PDF...");
+
+	pdf.create(html, pdfOptions).toFile( 
+		options.destination,
+		function(err) {
+			if (err) return console.log(err);
+		}
+	);
 };
+
+export default generatePDF;
