450b5f20b0f88b5a7e5de61799acbac7ae78eea9
mcclowes <maxcc@me.com>
2019-01-30 12:13:59 +0000

    test init

---
+++ src/__tests__/root.spec.js
@@ -0,0 +1,9 @@
+//import { mount, } from "enzyme";
+
+describe("Root", () => {
+	describe("should just pass", () => {
+		it("because it is true", () => {
+			expect(true).toBe(true);
+		});
+	});
+});
